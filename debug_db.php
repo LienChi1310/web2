c20dd6164a015076452ee72ccae26fb324e10e52
ngkhuee <ngockhue0710@gmail.com>
2026-03-27 20:01:32 +0700

    Remove cache, debug files and vendor directory

--- debug_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/admin/config/config.php';
-$r = $mysqli->query('SELECT DATABASE() AS db, NOW() as now');
-$row = $r->fetch_assoc();
-echo "DB đang dùng: {$row['db']}<br>Thời điểm: {$row['now']}<br>";
